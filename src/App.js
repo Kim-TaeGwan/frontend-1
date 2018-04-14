c156a56987d5ef7fac2f80c2b6823877edf0cc84
FULLNAM-VC1JIOU\Administrator <riahnrhyme@gmail.com>
2018-04-14 17:48:52 +0900

    rest api test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-	render(){
+    
+    constructor(){
+        super();
+        
+        this.state = {
+            title : '초기제목',
+            name : '초기이름'
+        }
+    }
+    
+    getCompany(){
+        //axios
+        axios.get('http://localhost:4000/company')
+            .then(response=>{
+                console.log(response.data);
+                this.setState({ 
+                    title : response.data.title,
+                    name : response.data.name
+                });
+            })
+            .catch(error=>{
+                console.log(error);
+            });
+    }
+
+    render(){
 		return (
-			<div>App</div>
+            <div>
+                <div>백엔드 통신 해보기</div>
+                <button onClick={this.getCompany.bind(this)}>
+                    통신시작</button>
+                <div>{this.state.title}</div>
+                <div>{this.state.name}</div>
+            </div>
 		)
 	}
 }
